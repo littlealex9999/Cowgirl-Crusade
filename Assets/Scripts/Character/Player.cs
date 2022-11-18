b279ff1b11d9f0cd4cc21d1ddbf85cf44c10335a
little <littlealex9999@gmail.com>
2022-11-18 15:21:06 +1100

    Add shoddy boundaries player movement

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -6,9 +6,14 @@ public class Player : Character
 {
     public float moveSpeed = 1;
 
+    public Vector2 boundaries;
+
+    Camera mainCamera;
+
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        CalculateBoundaries();
     }
 
     protected override void Update()
@@ -20,7 +25,25 @@ public class Player : Character
 
     void Move()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        gameObject.transform.localPosition += moveInput * moveSpeed * Time.deltaTime;
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed * Time.deltaTime;
+        if (transform.localPosition.x + moveInput.x > boundaries.x) {
+            moveInput.x = boundaries.x - transform.localPosition.x;
+        } else if (transform.localPosition.x + moveInput.x < -boundaries.x) {
+            moveInput.x = -boundaries.x - transform.localPosition.x;
+        }
+
+        if (transform.localPosition.y + moveInput.y > boundaries.y) {
+            moveInput.y = boundaries.y - transform.localPosition.y;
+        } else if (transform.localPosition.y + moveInput.y < -boundaries.y) {
+            moveInput.y = -boundaries.y - transform.localPosition.y;
+        }
+
+        gameObject.transform.localPosition += moveInput;
+    }
+
+    void CalculateBoundaries()
+    {
+        Vector3 v3ViewPort = new Vector3(1, 0, mainCamera.transform.localPosition.z);
+        boundaries = (mainCamera.ViewportToWorldPoint(v3ViewPort) - mainCamera.transform.position) * 2;
     }
 }
