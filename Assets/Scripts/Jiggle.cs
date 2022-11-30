c9de60e8823435179110ab1ac4f0724d7091255f
little <littlealex9999@gmail.com>
2022-11-30 15:40:38 +1100

    Fix unrealistic jiggle

--- Assets/Scripts/Jiggle.cs
+++ Assets/Scripts/Jiggle.cs
@@ -28,17 +28,20 @@ public class Jiggle : MonoBehaviour
         moved += finalMove;
 
         if (moved.x > Mathf.Abs(maxJiggle.x) || moved.x < -Mathf.Abs(maxJiggle.x)) {
-            finalMove.x -= (maxJiggle.x - moved.x);
+            finalMove.x = (maxJiggle.x - moved.x);
+            moved.x = maxJiggle.x;
             maxJiggle.x *= -1;
         }
 
         if (moved.y > Mathf.Abs(maxJiggle.y) || moved.y < -Mathf.Abs(maxJiggle.y)) {
-            finalMove.y -= (maxJiggle.y - moved.y);
+            finalMove.y = (maxJiggle.y - moved.y);
+            moved.y = maxJiggle.y;
             maxJiggle.y *= -1;
         }
 
         if (moved.z > Mathf.Abs(maxJiggle.z) || moved.z < -Mathf.Abs(maxJiggle.z)) {
-            finalMove.z -= (maxJiggle.z - moved.z);
+            finalMove.z = (maxJiggle.z - moved.z);
+            moved.z = maxJiggle.z;
             maxJiggle.z *= -1;
         }
 
