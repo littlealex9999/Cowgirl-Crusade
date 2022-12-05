b2fdd40db777d2c67eaee9230494dab42ec82f24
little <littlealex9999@gmail.com>
2022-12-05 16:38:09 +1100

    Add autonomy to set enemy shoot

--- Assets/Scripts/Character/SetEnemyTarget_PLAYER.cs
+++ Assets/Scripts/Character/SetEnemyTarget_PLAYER.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetEnemyTarget : MonoBehaviour
+public class SetEnemyTarget_PLAYER : MonoBehaviour
 {
     public Enemy[] enemiesToSet;
     public Character newTarget;
+
+    public bool justFigureOutWhoThePlayerIs;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && enemiesToSet != null) {
             foreach (Enemy e in enemiesToSet) {
-                e.SetTarget(newTarget);
+                if (justFigureOutWhoThePlayerIs) {
+                    e.SetTarget(other.GetComponentInChildren<Player>());
+                } else {
+                    e.SetTarget(newTarget);
+                }
             }
         }
     }
