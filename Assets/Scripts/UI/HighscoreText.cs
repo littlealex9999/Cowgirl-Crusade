9504dcc674a693fc9353f0ea509df091706810ee
little <littlealex9999@gmail.com>
2022-12-14 16:34:16 +1100

    Add Game Over

---
+++ Assets/Scripts/UI/HighscoreText.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HighscoreText : MonoBehaviour
+{
+    public Score scoreObject;
+    public TextMeshProUGUI highscores;
+    public TextMeshProUGUI currentScore;
+
+    public float timeToReturn = 10;
+
+    void Start()
+    {
+        highscores.text = "Highscores \n";
+
+        for (int i = 0; i < scoreObject.GetHighscores.Length; ++i) {
+            highscores.text += scoreObject.GetHighscores[i].ToString() + "\n";
+        }
+
+        currentScore.text = "Your Score \n" + scoreObject.GetPoints.ToString();
+
+        Invoke("ReturnToMainMenu", timeToReturn);
+    }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
